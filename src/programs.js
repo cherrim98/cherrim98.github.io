0685a43454afe468ffcf8f2595bd20b2734162f9
Aaron <cherrim98@gmail.com>
2023-09-20 20:16:31 -0400

    Update programs.js

--- src/programs.js
+++ src/programs.js
@@ -439,7 +439,7 @@ function Solitaire() {
 
 function Spider() {
 	var $win = make_iframe_window({
-		src: "programs/spider/spider.htm",
+		src: "programs/spider/spider.html",
 		icons: iconsAtTwoSizes("spider"),
 		title: "Spider Solitaire",
 		innerWidth: 585,
